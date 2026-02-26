28ccc4f3afc3268f97ace4b54354f1d0d38d7774
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2026-02-26 17:41:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,35 +4,27 @@ from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const container = document.getElementById("posts");
 
-if (container) {
+const snapshot = await getDocs(collection(db,"posts"));
 
-  try {
+snapshot.forEach(doc=>{
+  const post = doc.data();
 
-    const snapshot = await getDocs(collection(db, "posts"));
-
-    snapshot.forEach(doc => {
-
-      const post = doc.data();
-
-      if (post.status === "published") {
-
-        container.innerHTML += `
-          <div class="card">
-            <h2>
-              <a href="post.html?id=${doc.id}">
-                ${post.title}
-              </a>
-            </h2>
-            <p>${post.meta}</p>
-          </div>
-        `;
-      }
-
-    });
-
-  } catch (error) {
-    container.innerHTML = "Error loading posts.";
-    console.error(error);
+  if(post.status==="published"){
+    container.innerHTML += `
+      <div class="card">
+        <img src="${post.image}">
+        <h2>
+          <a href="post.html?slug=${post.slug}">
+            ${post.title}
+          </a>
+        </h2>
+        <p>${post.meta}</p>
+        <span class="category">${post.category}</span>
+      </div>
+    `;
   }
+});
 
+window.toggleDark = function(){
+  document.body.classList.toggle("dark");
 }
