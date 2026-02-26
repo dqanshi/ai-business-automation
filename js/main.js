ecc4eccbed6889dc8d72b5021a4a607398b9f931
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2026-02-26 15:32:14 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
 import { db } from "./firebase.js";
-import { collection, getDocs } 
+import { collection, getDocs }
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const container = document.getElementById("posts");
 
-const snapshot = await getDocs(collection(db,"posts"));
+if (container) {
 
-snapshot.forEach(doc=>{
-const post = doc.data();
+  try {
 
-if(post.status==="published"){
-container.innerHTML += `
-<div class="card">
-<h2><a href="post.html?id=${doc.id}">${post.title}</a></h2>
-<p>${post.meta}</p>
-</div>
-`;
+    const snapshot = await getDocs(collection(db, "posts"));
+
+    snapshot.forEach(doc => {
+
+      const post = doc.data();
+
+      if (post.status === "published") {
+
+        container.innerHTML += `
+          <div class="card">
+            <h2>
+              <a href="post.html?id=${doc.id}">
+                ${post.title}
+              </a>
+            </h2>
+            <p>${post.meta}</p>
+          </div>
+        `;
+      }
+
+    });
+
+  } catch (error) {
+    container.innerHTML = "Error loading posts.";
+    console.error(error);
+  }
+
 }
-});
