f160b1443eee5172eb416b675144a330f67cbde8
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2026-02-26 15:22:10 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,16 +1,26 @@
-import { db } from "./firebase.js";
+import { db } from "./js/firebase.js";
 import { collection, addDoc }
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-window.publish = async function(){
+document.getElementById("saveBtn").addEventListener("click", async () => {
+  try {
 
-await addDoc(collection(db,"posts"),{
-title:title.value,
-meta:meta.value,
-content:content.value,
-status:status.value,
-date:new Date().toISOString()
+    const title = document.getElementById("title").value;
+    const meta = document.getElementById("meta").value;
+    const content = document.getElementById("content").value;
+    const status = document.getElementById("status").value;
+
+    await addDoc(collection(db,"posts"),{
+      title,
+      meta,
+      content,
+      status,
+      date: new Date().toISOString()
+    });
+
+    alert("Saved successfully");
+
+  } catch (error) {
+    alert(error.message);
+  }
 });
-
-alert("Saved");
-}
