d808cfae6bfd9c10b4694aeaee661a7a46ad1910
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2026-02-26 12:48:42 +0530

    Add functionality to fetch and display posts

---
+++ js/main.js
@@ -0,0 +1,20 @@
+import { db } from "./firebase.js";
+import { collection, getDocs } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const container = document.getElementById("posts");
+
+const snapshot = await getDocs(collection(db,"posts"));
+
+snapshot.forEach(doc=>{
+const post = doc.data();
+
+if(post.status==="published"){
+container.innerHTML += `
+<div class="card">
+<h2><a href="post.html?id=${doc.id}">${post.title}</a></h2>
+<p>${post.meta}</p>
+</div>
+`;
+}
+});
